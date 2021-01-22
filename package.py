bc44aa5091f090aab57a68056dd249ceaf18b8dd
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2021-01-22 22:12:38 +0900

    Update package.py

--- package.py
+++ package.py
@@ -1,8 +1,8 @@
 import datetime
 import shutil
 
-baseFileName = 'ExampleRecipesForToLaserBlade-v4-'
+base_file_name = 'ExampleRecipesForToLaserBlade-v4-'
 
 date = datetime.date.today().strftime("%Y%m%d")
-zipname = 'out/' + baseFileName + date
-shutil.make_archive(zipname, 'zip', root_dir='src')
+zip_name = 'out/' + base_file_name + date
+shutil.make_archive(zip_name, 'zip', root_dir='src')
