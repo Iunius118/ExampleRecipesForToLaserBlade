146a1362b24a53ba7cfe22ed8719f2cd75735185
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2021-02-06 03:12:05 +0900

    Fix package.py to add if __name__ == '__main__'

--- package.py
+++ package.py
@@ -1,8 +1,12 @@
 import datetime
 import shutil
 
-base_file_name = 'ExampleRecipesForToLaserBlade-v4-'
+def main():
+    base_file_name = 'ExampleRecipesForToLaserBlade-v4-'
 
-date = datetime.date.today().strftime("%Y%m%d")
-zip_name = 'out/' + base_file_name + date
-shutil.make_archive(zip_name, 'zip', root_dir='src')
+    date = datetime.date.today().strftime('%Y%m%d')
+    zip_name = 'out/' + base_file_name + date
+    shutil.make_archive(zip_name, 'zip', root_dir='src')
+
+if __name__ == '__main__':
+    main()
